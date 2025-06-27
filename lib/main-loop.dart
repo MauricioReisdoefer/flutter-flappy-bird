2d9a8135880cf6c9d019d53de9de25a580c31bd0
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-06-26 22:30:21 -0300

    FEAT Flame Setup

---
+++ lib/main-loop.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+class FlappyBird extends FlameGame {
+  @override
+  Future<void> onLoad() async {
+
+  }
+
+  @override
+  void update(double dt) {
+    
+  }
+
+  @override
+  void render(Canvas canvas) {
+  
+  }
+}
